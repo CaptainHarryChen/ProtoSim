8abf1d5cf1b2dd130c5ffb136f9ac793a2661348
CaptainHarryChen <captainharrychen@qq.com>
2026-03-04 12:18:33 +0800

    better geometry shader for capsule

--- viewer/shader/capsule_raycast.gs.glsl
+++ viewer/shader/capsule_raycast.gs.glsl
@@ -55,50 +55,31 @@ void main()
     vec3 basisX, basisY;
     buildTangentBasis(dirToCam, basisX, basisY);
 
-    vec4 topCenter = u_projMatrix * vec4(topSphereCenter + dirToCam * radius, 1.0);
-    vec4 bottomCenter = u_projMatrix * vec4(bottomSphereCenter + dirToCam * radius, 1.0);
+    vec4 topClip    = u_projMatrix * vec4(topSphereCenter, 1.0);
+    vec4 bottomClip = u_projMatrix * vec4(bottomSphereCenter, 1.0);
+    vec2 topNDC    = topClip.xy / topClip.w;
+    vec2 bottomNDC = bottomClip.xy / bottomClip.w;
 
-    vec4 dx = u_projMatrix * vec4(basisX * radius, 0.0);
-    vec4 dy = u_projMatrix * vec4(basisY * radius, 0.0);
+    float projScale = u_projMatrix[1][1];
+    float topRadiusScreen =
+        radius * projScale / -topSphereCenter.z;
+    float bottomRadiusScreen =
+        radius * projScale / -bottomSphereCenter.z;
+    vec2 dir = normalize(bottomNDC - topNDC);
+    vec2 normal = vec2(-dir.y, dir.x);
 
-    vec4 topP1 = topCenter - dx - dy;
-    vec4 topP2 = topCenter + dx - dy;
-    vec4 topP3 = topCenter - dx + dy;
-    vec4 topP4 = topCenter + dx + dy;
+    vec2 p1 = topNDC    + normal * topRadiusScreen - dir * topRadiusScreen;
+    vec2 p2 = topNDC    - normal * topRadiusScreen - dir * topRadiusScreen;
+    vec2 p3 = bottomNDC + normal * bottomRadiusScreen + dir * bottomRadiusScreen;
+    vec2 p4 = bottomNDC - normal * bottomRadiusScreen + dir * bottomRadiusScreen;
 
-    vec4 bottomP1 = bottomCenter - dx - dy;
-    vec4 bottomP2 = bottomCenter + dx - dy;
-    vec4 bottomP3 = bottomCenter - dx + dy;
-    vec4 bottomP4 = bottomCenter + dx + dy;
-
-    vec4 vertices[8] = vec4[](topP1, topP2, topP3, topP4, bottomP1, bottomP2, bottomP3, bottomP4);
-
-    vec2 minScreen = vertices[0].xy / vertices[0].w;
-    vec2 maxScreen = vertices[0].xy / vertices[0].w;
-    for (int i = 1; i < 8; ++i)
-    {
-        vec2 screen = vertices[i].xy / vertices[i].w;
-        minScreen = min(minScreen, screen);
-        maxScreen = max(maxScreen, screen);
-    }
-
-    float minZ = vertices[0].z / vertices[0].w;
-    for (int i = 1; i < 8; ++i)
-    {
-        float z = vertices[i].z / vertices[i].w;
-        minZ = min(minZ, z);
-    }
-
-    vec4 p1 = vec4(minScreen.x, minScreen.y, minZ, 1.0);
-    vec4 p2 = vec4(maxScreen.x, minScreen.y, minZ, 1.0);
-    vec4 p3 = vec4(minScreen.x, maxScreen.y, minZ, 1.0);
-    vec4 p4 = vec4(maxScreen.x, maxScreen.y, minZ, 1.0);
+    float minZ = min(topClip.z / topClip.w, bottomClip.z / bottomClip.w);
 
     v_capsuleCenterView = capsuleCenterViewPos;
     v_capsuleAxisView = capsuleAxisView;
     v_radiusView = radius;
     v_halfHeightView = halfHeight;
-    gl_Position = p1;
+    gl_Position = vec4(p1, minZ, 1.0);
     FragPos = gs_in[0].FragPos;
     albedoIn = gs_in[0].albedoIn;
     EmitVertex();
@@ -107,7 +88,7 @@ void main()
     v_capsuleAxisView = capsuleAxisView;
     v_radiusView = radius;
     v_halfHeightView = halfHeight;
-    gl_Position = p2;
+    gl_Position = vec4(p2, minZ, 1.0);
     FragPos = gs_in[0].FragPos;
     albedoIn = gs_in[0].albedoIn;
     EmitVertex();
@@ -116,7 +97,7 @@ void main()
     v_capsuleAxisView = capsuleAxisView;
     v_radiusView = radius;
     v_halfHeightView = halfHeight;
-    gl_Position = p3;
+    gl_Position = vec4(p3, minZ, 1.0);
     FragPos = gs_in[0].FragPos;
     albedoIn = gs_in[0].albedoIn;
     EmitVertex();
@@ -125,7 +106,7 @@ void main()
     v_capsuleAxisView = capsuleAxisView;
     v_radiusView = radius;
     v_halfHeightView = halfHeight;
-    gl_Position = p4;
+    gl_Position = vec4(p4, minZ, 1.0);
     FragPos = gs_in[0].FragPos;
     albedoIn = gs_in[0].albedoIn;
     EmitVertex();
